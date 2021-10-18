cfed56ce3970ee075ad90523966fd43eab91b16a
HugoHernandez06 <hugoa.hernandez@upb.edu.co>
2021-10-17 20:32:12 -0700

    Funciones por consola / Bug con la comunicion con cliente

--- server.c
+++ server.c
@@ -12,28 +12,89 @@
 
 #define SERVER_FIFO "/tmp/addition_fifo_server"
 
-void funcExit(char *buffer);
-void funcAdd(char *buffer);
-void funcRemove(char *buffer);
-void funcTrigger(char *buffer);
-void funcList(char *buffer);
+typedef struct{
+    char event_name[100];
+    char clients[100];
+}events;
+
+char event[10][10] = {"null","null","null","null","null","null","null","null","null","null"};
+char *buffer;
+char comando[100];
+
+int fd, fd_client, bytes_read;
+char buf [4096];
+char bufCmd [100];
+char *return_fifo;
+
+void funcExit();
+void funcAdd(char *resp);
+void funcRemove(char *resp);
+void funcTrigger(char *resp);
+void funcList();
+void *threadComandos(void *arg);
+
+void *threadComandos(void *arg){
+    while (1)
+    {
+        fgets(comando,sizeof(comando),stdin);
+        for (int i = 0; comando[i] !='\0'; ++i)
+        {
+            comando[i] = tolower(comando[i]);
+        }
+        char *token = strtok(comando," ");
+        char *action = NULL;
+        char *resp;
+        for (int i = 0; token!= NULL; i++)
+        {
+            if (i == 0)
+            {
+                action = token;
+            }else if (i == 1)
+            {
+                resp = token;
+            }
+            token = strtok(NULL," ");
+        }
+
+        int exit = strcmp(action,"exit\n");
+        int add = strcmp(action,"add");
+        int remove = strcmp(action,"remove");
+        int trigger = strcmp(action,"trigger");
+        int list = strcmp(action,"list\n");
+
+        if (exit == 0)
+        {
+            funcExit();
+        }else if (add == 0)
+        {
+            funcAdd(resp);
+        }else if (remove == 0)
+        {
+            funcRemove(resp);
+        }else if (trigger == 0)
+        {
+            funcTrigger(resp);
+        }else if (list == 0)
+        {
+            funcList();
+        }
+    }
+    return NULL;
+}
 
 int main (int argc, char **argv)
 {
-    int fd, fd_client, bytes_read;
-    char buf [4096];
-    char bufCmd [100];
-    char *return_fifo;
-
+    pthread_t threadsID;
+    pthread_create(&threadsID,NULL,&threadComandos, NULL);
     if ((mkfifo (SERVER_FIFO, 0664) == -1) && (errno != EEXIST)) {
         perror ("mkfifo");
         exit (1);
     }
     if ((fd = open (SERVER_FIFO, O_RDONLY)) == -1){
         perror ("open");
-    }
-    printf("Welcome to Juan & Hugo's server =D\n\n"); 
-
+    }else {
+        printf("Welcome to Juan & Hugo's server =D\n\n"); }
+    
     while (1) {
         // Get a message
         memset (buf, '\0', sizeof (buf));
@@ -62,23 +123,57 @@ int main (int argc, char **argv)
 
             if (close (fd_client) == -1)
                 perror ("close");
-        }
+        } 
     }
+    
+
     return(EXIT_SUCCESS);
 }
 
-void funcExit(char *buffer){
-    printf("Soy Exit");
+void funcExit(){
+    printf("Soy Exit\n");
+    funcTrigger("exit");
+    exit(0);
 }
-void funcAdd(char *buffer){
-    printf("Soy Add");
+void funcAdd(char *resp){
+    printf("Soy Add\n");
+    for (int i = 0; i < 10; i++)
+    {
+        if (strcmp(event[i],"null") == 0)
+        {
+            strcpy(event[i], resp);
+            printf("Agrega %d: %s",i,event[i]);
+            break;
+        }
+    }
 }
-void funcRemove(char *buffer){
-    printf("Soy Remove");
+void funcRemove(char *resp){
+    printf("Soy Remove\n");
 }
-void funcTrigger(char *buffer){
-    printf("Soy Trigger");
+void funcTrigger(char *resp){
+    printf("Soy Trigger\n");
+    if (bytes_read > 0) {
+            return_fifo = strtok (resp, ", \n");
+            /* Send the result */
+            if ((fd_client = open (return_fifo, O_WRONLY)) == -1) {
+                perror ("open: client fifo");
+            }   
+                printf("Console: ");
+                fgets(bufCmd,30,stdin);
+                printf("\n");
+                sprintf (resp, "Response of Server:  %s\n", bufCmd);
+
+            if (write (fd_client, resp, strlen (resp)) != strlen (resp))
+                perror ("write");
+
+            if (close (fd_client) == -1)
+                perror ("close");
+        } 
 }
-void funcList(char *buffer){
-    printf("Soy List");
+void funcList(){
+    printf("Soy List\n");
+    for (int i = 0; i < 10; i++)
+    {
+        printf("Eventos %d: %s\n",i,event[i]);
+    }
 }
